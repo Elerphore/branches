65d4c0c8fe7ef602f54a34fdeea2dfbfc4353aca
Eremey Malinovsky <Elerphore@gmail.com>
2023-01-17 18:22:56 +0500

    *

---
+++ rectangles/target/debug/deps/rectangles-15c485f4741d4751.d
@@ -0,0 +1,5 @@
+/home/eler/projects/branches/rectangles/target/debug/deps/rectangles-15c485f4741d4751: src/main.rs
+
+/home/eler/projects/branches/rectangles/target/debug/deps/rectangles-15c485f4741d4751.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/eler/projects/branches/rectangles/target/debug/rectangles: /home/eler/projects/branches/rectangles/src/main.rs
