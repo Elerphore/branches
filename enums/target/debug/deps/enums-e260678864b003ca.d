ea8116c02e556bb3abf6d22c2cc14b1c9665b0ce
Eremey Malinovsky <Elerphore@gmail.com>
2023-01-17 21:21:48 +0500

    *

---
+++ enums/target/debug/deps/enums-e260678864b003ca.d
@@ -0,0 +1,5 @@
+/home/eler/projects/branches/enums/target/debug/deps/enums-e260678864b003ca: src/main.rs
+
+/home/eler/projects/branches/enums/target/debug/deps/enums-e260678864b003ca.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+/home/eler/projects/branches/enums/target/debug/enums: /home/eler/projects/branches/enums/src/main.rs
