45047116a1c0a0156256a2e7a6bd5890d7d23714
Eremey Malinovsky <Elerphore@gmail.com>
2023-01-17 18:03:10 +0500

    *

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,10 +5,12 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+
 fn build_user(email: String, username: String) -> User {
     User {
-        username: username,
-        email: email,
+        username,
+        email,
         active: true,
         sign_in_count: 1,
     }
@@ -16,6 +18,13 @@ fn build_user(email: String, username: String) -> User {
 
 fn main() {
     let user = build_user(String::from("some_funny_email@gmail.com"), String::from("really_cool_username"),);
+
+    let user_2 = User {
+        email: String::from("Oh.No.@gmail.com"),
+        ..user
+    };
+
+    let color_1 = Color(10, 10, 10);
 
     println!("{}", String::from(user));
 }
