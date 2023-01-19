0ef0cbd5b292508837d43587897028d72c9819dd
Eremey Malinovsky <Elerphore@gmail.com>
2023-01-19 20:43:26 +0500

    *

--- options/src/main.rs
+++ options/src/main.rs
@@ -1,10 +1,30 @@
+use crate::Coin::{Nickel, Penny};
+
 enum Option<T> {
     None,
     Some(T),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
 fn main() {
     let some_number = Some(5);
     let some_char = Some('h');
     let absent_number: Option<i32> = Option::None;
+
+    dbg!(value_in_cents(Nickel));
 }
