fc1d80b13d906377f57c7a7a361bda2805705a9c
Eremey Malinovsky <Elerphore@gmail.com>
2023-01-20 17:51:49 +0500

    *

--- options/src/main.rs
+++ options/src/main.rs
@@ -14,7 +14,10 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("lucky penny");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
